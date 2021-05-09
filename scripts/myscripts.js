be111a27c1c7ebc63a17a9f1cc13666327a64a20
José Pedro Faria <josepedropfaria@gmail.com>
2021-05-09 21:19:35 +0100

    Adicionei Tabs personalizadas

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function opentab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
   
@@ -15,8 +15,7 @@ function openCity(evt, cityName) {
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
 
-  
